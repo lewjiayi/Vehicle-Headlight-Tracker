3bd62b389ef37a8d13e5a4ed4a41987dcd38018a
lewjiayi <jiaareaper@hotmail.com>
2019-11-06 09:54:34 +0800

    improved loop control

--- blob.py
+++ blob.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class Blob():
-    def __init__(self, index, contour, minVal, maxVal, moment):
+    def __init__(self, index, contour, minVal, maxVal, area):
         self.index = index
         self.contour = contour
         self.minVal = minVal
@@ -12,10 +12,10 @@ class Blob():
                     [maxVal[0] + 20, minVal[1] - 20], 
                     [maxVal[0] + 20, maxVal[1] + 20], 
                     [minVal[0] - 20, maxVal[1] + 20]]
-        self.moment = moment
+        self.area = area
         self.missingCount = 0
 
-    def update(self, contour, minVal, maxVal, moment):
+    def update(self, contour, minVal, maxVal, area):
         self.contour = contour
         self.minVal = minVal
         self.maxVal = maxVal
@@ -25,7 +25,7 @@ class Blob():
                     [maxVal[0] + 20, minVal[1] - 20], 
                     [maxVal[0] + 20, maxVal[1] + 20], 
                     [minVal[0] - 20, maxVal[1] + 20]]
-        self.moment =moment
+        self.area =area
 
     def missing(self):
         self.missingCount += 1
--- detct.py
+++ detct.py
@@ -7,6 +7,7 @@ from blob import Blob
 
 cap = cv2.VideoCapture('stock_video/Pexels Videos 2053100.mp4')
 blobs = []
+index = 0
 while (cap.isOpened()):
     ret, frame = cap.read()
     if ret:
@@ -19,74 +20,84 @@ while (cap.isOpened()):
         # blur = cv2.blur(gray, (5,5))
         # _, t = cv2.threshold(blur, 220, 255, cv2.THRESH_BINARY)
         # contours, hierarchy = cv2.findContours(t, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        print("Blob count in this frame")
-        print(len(contours))
-        updatedBlob = []
-        for blob in blobs:
-            if blob.missingCount >= 1:
-                continue
-            badBlob = []
+
+        # Remove Bad Blod
+        badBlob = []
+        for countCnt, cnt in enumerate(contours, start=0):
+            low = np.min(np.max(cnt,axis=1),axis=0)
+            high = np.max(np.max(cnt,axis=1),axis=0)
+            x = high[0] - low[0]
+            y = high[1] - low[1]
+            # Check if contour is singular point
+            if (x == 0) or (y == 0):
+                badBlob.append(countCnt)
+            # Check for very small blob
+            elif (abs(x) < 3) or (abs(y) < 3):
+                badBlob.append(countCnt)
+            if cv2.contourArea(cnt) == 0:
+                badBlob.append(countCnt)
+            # elif (x/y < 0.5) or (x/y > 2):
+            #     badBlob.append(countCnt)
+            #     continue
+            # Check if cnt in range
+        contours = np.delete(contours, badBlob)
+
+        # Update Blob movement
+        updatedBlobs = []
+        missingBlobs = []
+        for countBlob, blob in enumerate(blobs, start=0):
             # Check previous frame blob saved with new frame blob
             for countCnt, cnt in enumerate(contours, start=0):
                 isUpdated = False
+                # Check if area moved only by small range
                 low = np.min(np.max(cnt,axis=1),axis=0)
                 high = np.max(np.max(cnt,axis=1),axis=0)
-                x = high[0] - low[0]
-                y = high[1] - low[1]
-                # Check if contour is singular point
-                if (x == 0) or (y == 0):
-                    badBlob.append(countCnt)
-                    continue
-                elif (abs(x) < 3) or (abs(y) < 3):
-                    badBlob.append(countCnt)
-                    continue
-                # elif (x/y < 0.5) or (x/y > 2):
-                #     badBlob.append(countCnt)
-                #     continue
-                # Check if cnt in range
                 if (low[0] not in range(blob.box[0][0], blob.box[1][0]) or
                     high[0] not in range(blob.box[0][0], blob.box[1][0]) or
                     low[1] not in range(blob.box[0][1], blob.box[2][1]) or
                     high[1] not in range(blob.box[0][1], blob.box[2][1])):
                     continue
-                ret = cv2.matchShapes(blob.contour, cnt, 2, 0)
                 # Check if cnt is similar
+                # ret = cv2.matchShapes(blob.contour, cnt, 2, 0)
                 # if ret >= 0.5:
                     # continue
-                # Check if cnt moved only by small range
-                mBlob = cv2.moments(blob.contour)
-                mCnt = cv2.moments(cnt)
-
-                blob.update(cnt, low, high, mCnt)
-                updatedBlob.append(blob)
+                # Check if area is similar
+                areaCnt = cv2.contourArea(cnt)
+                if ((blob.area/areaCnt) >= 1.2 or
+                    (blob.area/areaCnt) <= 0.8 ):
+                    continue
+                blob.update(cnt, low, high, areaCnt)
+                updatedBlobs.append(blob)
                 isUpdated = True
                 break
+            # Remove Matched Contour and flag missing blob
             if isUpdated:
                 if (len(contours) > 0):
                     contours = np.delete(contours, countCnt)
             else:
-                blob.missing()
-            contours = np.delete(contours, badBlob)
+                missingBlobs.append(countBlob)
+
+        for i in sorted(missingBlobs, reverse=True):
+            del blobs[i]
         
-
-
-        print("New blob count in this frame")
-        print(len(contours))
+        # print("New blob count in this frame")
+        # print(len(contours))
         # Add newly detected blob into list
         if (len(contours) > 0):
             for cnt in contours:
+                index += 1
                 low = np.min(np.max(cnt,axis=1),axis=0)
                 high = np.max(np.max(cnt,axis=1),axis=0)
-                m = cv2.moments(cnt)
-                blobs.append(Blob(len(blobs), cnt, low, high, m))
-                updatedBlob.append(Blob(len(blobs)-1, cnt, low, high, m))    
+                area = cv2.contourArea(cnt)
+                blobs.append(Blob(index, cnt, low, high, area))
+                updatedBlobs.append(Blob(index, cnt, low, high, area)) 
 
-        for blob in updatedBlob:
-            cv2.putText(resized, str(blob.index), 
+        for blob in updatedBlobs:
+            cv2.putText(resized, str(blob.index),
                 (int((blob.minVal[0]+blob.maxVal[0])/2), blob.maxVal[1] - 20),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
             # cv2.drawContours(resized, [blob.contour], -1, (0,0,0), 3)
-        cv2.rectangle(resized, (blobs[0].box[0][0], blobs[0].box[0][1]), (blobs[0].box[2][0], blobs[0].box[2][1]), (0,0,0), 2)
+        # cv2.rectangle(resized, (blobs[0].box[0][0], blobs[0].box[0][1]), (blobs[0].box[2][0], blobs[0].box[2][1]), (0,0,0), 2)
 
         cv2.imshow('hsv', resized)
         
